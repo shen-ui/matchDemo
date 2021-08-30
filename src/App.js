1c29522f1873003d39950abf45c0ea56f4784a92
shen-ui <peterhuiwang1995@gmail.com>
2021-08-29 20:10:39 -0400

    removed XMLParser for xml-js. Better features and output is correctly parsed.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import './App.css';
 
+const convert = require("xml-js");
 
 function App() {
 
-  const [data, setData] = useState(0);
+  const [data, setData] = useState();
 
-  // useEffect updates dom when an event happens
   useEffect(() => {
     axios
       .get(' https://static.yinzcam.com/interviews/web/api/match1.xml', {
         'Content-Type': "applications/xml"
       })
-      .then(function(res){
-        console.log(res.data);
-        setData(res.data);
+      .then((res) => {
+        const parser = JSON.parse(
+          convert.xml2json(res.data, {compact: true, spaces: 2})
+        );
+        console.log(parser.Formation);
+        // potentially need to use more states
+        setData(parser);
       })
-  });
+      .catch((err) => {
+        console.log("error: ", err);
+      })
+  }, []);
 
   return (
     <div className="App">
-      {data}
+      {}
     </div>
   );
 }
