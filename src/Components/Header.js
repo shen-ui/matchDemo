4fae02cb3717f8bc7ce9b2044fa42f0038a4dc15
shen-ui <peterhuiwang1995@gmail.com>
2021-09-06 10:41:55 -0400

    minor correction in time on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,13 +6,17 @@ const Header = ({timeStamp, location}) => {
         let hour = timeStamp.slice(11,13);
         let day = timeStamp.slice(5,7);
         //remove 0 if day contains 0
-        if(day.slice(0,1) === '0'){
+        if(day.slice(0,1) == '0'){
            day = day.slice(1,2);
         }
         //set up meridian and hour
-        if(hour >= 12){
-            hour -= 12;
+
+        if(hour == 12){
             merid = "PM";
+        }
+        else if(hour > 12){
+            hour -= 12;
+            merid = "PM"
         }
         else merid = 'AM';
         
